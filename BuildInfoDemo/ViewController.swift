3e4d213cace4be721dd7f774d1bf503467f35244
EyreFree <eyrefree@163.com>
2016-03-29 16:59:01 +0800

    Initial Commit

---
+++ BuildInfoDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuildInfoDemo
+//
+//  Created by MBP on 16/3/29.
+//  Copyright © 2016年 MBP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
