eb57c3ffccf739a8dac384212be792a091d9825e
EyreFree <eyrefree@EyreFreedeMacBook-Pro.local>
2017-03-14 16:13:26 +0800

    Swift 3

--- EFBuildInfoDemo/ViewController.swift
+++ EFBuildInfoDemo/ViewController.swift
@@ -1,9 +1,9 @@
 //
 //  ViewController.swift
-//  BuildInfoDemo
+//  EFBuildInfoDemo
 //
-//  Created by EyreFree on 16/3/29.
-//  Copyright © 2016年 eyrefree. All rights reserved.
+//  Created by EyreFree on 17/3/14.
+//  Copyright © 2017年 eyrefree. All rights reserved.
 //
 
 import UIKit
@@ -13,12 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         //输出构建信息
-        var bulidTime: String!
-        var gitSha: String!
-        if let buildConfigFilePath = NSBundle.mainBundle()
-            .pathForResource("BuildConfig", ofType: "plist") {
+        var bulidTime = ""
+        var gitSha = ""
+        if let buildConfigFilePath = Bundle.main.path(forResource: "BuildConfig", ofType: "plist") {
             if let dict = NSDictionary(contentsOfFile: buildConfigFilePath) {
                 bulidTime = dict["BUILD_TIME"] as? String ?? "未知"
                 gitSha = dict["GIT_SHA"] as? String ?? "未知"
--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# BuildInfoDemo
+# EFBuildInfoDemo
 
-iOS 在 App 中获取当前版本的构建时间和 Git Hash 值
+iOS 在 App 中获取当前版本的构建时间和 Git Hash 值 Demo
 
-具体介绍参见：[http://www.eyrefree.org/2016/03/08/2016-03-08-iOS-Build-Info/](http://www.eyrefree.org/2016/03/08/2016-03-08-iOS-Build-Info/)
+## 环境
+
+- XCode 8.0+
+- Swift 3.0+
+
+## 详细介绍
+
+参见：[http://www.eyrefree.org/2016/03/08/2016-03-08-iOS-Build-Info/](http://www.eyrefree.org/2016/03/08/2016-03-08-iOS-Build-Info/)
+
+## 作者
+
+EyreFree, eyrefree@eyrefree.org
+
+## 协议
+
+EFBuildInfoDemo 基于 MIT 协议进行分发和使用，更多信息参见协议文件。
